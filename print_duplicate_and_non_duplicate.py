350a81d4dbb34578dff44d8893e4ae598cf2eee8
Aeron <dequitoaeron353@gmail.com>
2025-03-13 01:00:47 +0800

    made a list, while loop, used try, input numbers, and append

--- print_duplicate_and_non_duplicate.py
+++ print_duplicate_and_non_duplicate.py
@@ -1,6 +1,14 @@
 # Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid.
 # Display "Unique" after input when the inputted number don't have duplicate. 
 # Display "Duplicate" after input when the inputted number have duplicate.
+
+# Make a list and count
+count = 0
+nums = []
 # Input a number until the input is invalid
+while True:
+    try:
+        number = float(input(f"Enter Number {count}: "))
+        count += 1
 # Print Unique if it don't have duplicate
-# Print Duplicate if it has
+# Print Duplicate if it is
