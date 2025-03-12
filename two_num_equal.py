694c62bb6caca434f57bd1bf09b84acfcade01de
Aeron <dequitoaeron353@gmail.com>
2025-03-12 17:33:35 +0800

    made the user input part

--- two_num_equal.py
+++ two_num_equal.py
@@ -1,4 +1,8 @@
 # Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+
 # Ask for user input
-# Determie if equal
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
+
+# Determine if equal
 # Print the output
