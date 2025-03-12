23844738fc47d1964d4c4d53de9bc2f0009706e7
Aeron <dequitoaeron353@gmail.com>
2025-03-12 22:12:37 +0800

    list anf input number part

--- even_counter.py
+++ even_counter.py
@@ -1,6 +1,12 @@
 # Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
+
 # Make an empty list
+even_list = []
+
 # ASk for user input
+for ask in range(1, 11):
+    answer = float(input(f"Enter number {ask}: "))
+
 # Filter out the even nums
 # Get the taotal number of even
 # Print the total
