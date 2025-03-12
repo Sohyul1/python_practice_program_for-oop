dbb6a3c68c9cb281473df3388d3ac546083d9a06
Aeron <dequitoaeron353@gmail.com>
2025-03-12 17:28:23 +0800

    used max() to find the bigger and then print

--- print_bigger_num.py
+++ print_bigger_num.py
@@ -1,8 +1,11 @@
 # Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
 
 # Ask for user input
-num1 = float(input("Enter the 1st num: "))
-num2 = float(input("Enter the 2nd num: "))
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
 
 # Determine the bigger number
+bigger = max(num1, num2)
+
 # Print the bigger number
+print(f"{bigger} is the bigger number.")
