cc4c49469d90230b80a8dbbf0ddca701c0f08946
Aeron <dequitoaeron353@gmail.com>
2025-03-13 07:56:47 +0800

    pseudocode

---
+++ continue_asking_print_highest_number.py
@@ -0,0 +1,5 @@
+# Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
+# Make an empty list
+# Ask for user input
+# Determine the highest number 
+# Print the highest number
