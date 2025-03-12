7fc8967a58c5f33ddb83bc540f5d4cf3c6a2fcc9
Aeron <dequitoaeron353@gmail.com>
2025-03-12 18:45:38 +0800

    print total of 10 nums

--- sum_of_10_inputs.py
+++ sum_of_10_inputs.py
@@ -1,16 +1,15 @@
 # Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
 
 # Create an empty list for storing nums
-nums = []
+nums_storage = []
 
 # Ask for user input
 for ask in range (1,11):
     ans = float(input(f"Enter number{ask}:"))
     # Add the inputs in the empty list
-    nums.append(ans)
+    nums_storage.append(ans)
 
 # Get the sum of all of them
-sum(nums)
-
+total = sum(nums_storage)
 # Print the sum 
-print(sum)
+print(total)
