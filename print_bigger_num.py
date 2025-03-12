d512da64f3b322708008b9e9bad5fb07ca080c24
Aeron <dequitoaeron353@gmail.com>
2025-03-12 17:24:16 +0800

    made the user input part

--- print_bigger_num.py
+++ print_bigger_num.py
@@ -1,4 +1,8 @@
 # Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
+
 # Ask for user input
+num1 = float(input("Enter the 1st num: "))
+num2 = float(input("Enter the 2nd num: "))
+
 # Determine the bigger number
 # Print the bigger number
