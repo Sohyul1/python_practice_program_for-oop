40a5677a58a876cd44a354403482e40860e788eb
Aeron <dequitoaeron353@gmail.com>
2025-03-12 22:33:44 +0800

    list, count and while loop made

--- odd_nums_zero_to_hudred.py
+++ odd_nums_zero_to_hudred.py
@@ -1,7 +1,14 @@
 # Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
+
 # Make an empty list
+odd_list = []
+
 # Make a counter
-# Ask for user input using while loop
-# Filter out the odd numbers
-# Append odd numbers in the list
-# Print the odd numbers
+count = 0
+
+# Make the while loop
+while count < 100:
+
+    # Filter out the odd numbers
+    # Append odd numbers in the list
+    # Print the odd numbers
