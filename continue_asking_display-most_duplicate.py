b66ab00d9703e56815bd48d6790ded59b76ade0c
Aeron <dequitoaeron353@gmail.com>
2025-03-13 07:13:27 +0800

    pseudocode

---
+++ continue_asking_display-most_duplicate.py
@@ -0,0 +1,5 @@
+# Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
+# Make an empty list
+# Ask for user input
+# Get the most duplicated number
+# Print the number
