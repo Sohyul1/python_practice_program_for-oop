0f46b373c9de69d399e020dd0ba2e04b475ea102
Aeron <dequitoaeron353@gmail.com>
2025-03-12 23:16:28 +0800

    list, user input, and appending part

--- print_no_duplicate.py
+++ print_no_duplicate.py
@@ -1,9 +1,13 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+
 # Make an empty list 
+unique_num = []
 
 # Ask for user input 
-
-# Append the number in the list 
+for ask in range(1, 11):
+    nums = float(input(f"Enter number {ask}: "))
+    # Append the number in the list 
+    unique_num.append(nums)
 
 # Filter out numbers that appear only once 
 # Print the numbers that appear only once
