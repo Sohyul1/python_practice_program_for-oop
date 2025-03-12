a995a20ab35d00b53d180282184ddad8268f4384
Aeron <dequitoaeron353@gmail.com>
2025-03-12 20:50:13 +0800

    pseudocode

---
+++ two_num_not_equal.py
@@ -0,0 +1,4 @@
+# Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+# Ask for user input
+# Determine if equal or not
+# Print the output
