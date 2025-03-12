4cec210e981662ccc272f119185fa73fdcb00e01
Aeron <dequitoaeron353@gmail.com>
2025-03-13 07:17:13 +0800

    list and input numbres part

--- continue_asking_display-most_duplicate.py
+++ continue_asking_display-most_duplicate.py
@@ -1,5 +1,18 @@
 # Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
+
 # Make an empty list
+num_list = []
+
 # Ask for user input
+while True:
+    try:
+        num = float(input("Enter number: "))
+        num_list.append(num)
+
+
+    except ValueError:
+        break
+
 # Get the most duplicated number
+
 # Print the number
