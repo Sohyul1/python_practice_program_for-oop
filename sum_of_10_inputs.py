2b28fdd54bb7c820f7392d240ef01dc8c55732cb
Aeron <dequitoaeron353@gmail.com>
2025-03-12 18:42:12 +0800

    made an empty list and used append to add the inputs in the list

--- sum_of_10_inputs.py
+++ sum_of_10_inputs.py
@@ -1,8 +1,16 @@
 # Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
 
+# Create an empty list for storing nums
+nums = []
+
 # Ask for user input
 for ask in range (1,11):
     ans = float(input(f"Enter number{ask}:"))
+    # Add the inputs in the empty list
+    nums.append(ans)
 
 # Get the sum of all of them
+sum(nums)
+
 # Print the sum 
+print(sum)
