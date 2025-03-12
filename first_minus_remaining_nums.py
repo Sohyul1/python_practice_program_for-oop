75ebe663eac76a09ce1af11af9021156e1230607
Aeron <dequitoaeron353@gmail.com>
2025-03-12 21:37:50 +0800

    pseudocode

---
+++ first_minus_remaining_nums.py
@@ -0,0 +1,5 @@
+# Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
+# Make an empty list
+# Ask user for input using for loop
+# Get the difference
+# Print the difference
