f653a2a4d035e491eff990bc28115654c5eb555c
Aeron <dequitoaeron353@gmail.com>
2025-03-12 20:45:00 +0800

    input 2 numbers part

--- print_smaller_num.py
+++ print_smaller_num.py
@@ -1,4 +1,9 @@
 # Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
+
 # Ask for user input
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
+
+
 # Determine the smaller number
 # Print the number
