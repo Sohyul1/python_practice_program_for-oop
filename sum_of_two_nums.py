9845854e3747fa05ce76ea08f963529741a2bc6d
Aeron <dequitoaeron353@gmail.com>
2025-03-12 17:42:13 +0800

    made te input part

--- sum_of_two_nums.py
+++ sum_of_two_nums.py
@@ -1,4 +1,9 @@
 # Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+
 # Ask for user input
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
+
+
 # Get the sum
 # Print the total/sum
