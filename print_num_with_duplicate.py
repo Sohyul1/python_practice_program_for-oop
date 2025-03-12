b3bb4af9a4d7922b8e1ffaa3390aab6ae15766b8
Aeron <dequitoaeron353@gmail.com>
2025-03-13 06:49:12 +0800

    pseudocode

---
+++ print_num_with_duplicate.py
@@ -0,0 +1,6 @@
+# Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
+# Make an empty list
+# Ask for user input
+# Make A list of duplicates
+# Get the number that duplicates
+# Print the numbers that duplicates
