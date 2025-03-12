87bdb8bca370ab8a5c8d9907a61df740eb66a714
Aeron <dequitoaeron353@gmail.com>
2025-03-12 18:26:10 +0800

    input base and exponent part

--- num1_raised_to_num2.py
+++ num1_raised_to_num2.py
@@ -1,4 +1,8 @@
 # Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+
 # Ask for user input 
-# Get the nth-square of the first name (second num is the exponent)
+num1 = float(input("Enter the base number: "))
+num2 = float(input("Enter the exponent: "))
+
+# Get the nth-square of the first number (second num is the exponent)
 # Print the value
