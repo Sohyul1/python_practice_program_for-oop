6e30262c35ac7062e5a95c9b19bad7b684bb77ef
Aeron <dequitoaeron353@gmail.com>
2025-03-13 01:51:58 +0800

    I just copied this part from the last program (batch3 prog4) since they are Identical in this part

--- continue_asking_lowest_to_highest.py
+++ continue_asking_lowest_to_highest.py
@@ -1,6 +1,18 @@
 # Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function
+
 # Make a count variable 
+count = 1
+
 # Make an empty list
+num_list = []
+
 # Ask user for input (break if input is invalid)
-# Append the numbers in the list
+while True;
+    try:
+        number = float(input(f"Enter Number {count}: "))
+        count += 1
+        
+        # Append the numbers in the list
+        num_list.append(number)
+
 # Print the sorted list
