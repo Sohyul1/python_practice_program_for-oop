bde9a52779aeb9784f350b3191c74cc95bce9a63
Aeron <dequitoaeron353@gmail.com>
2025-03-12 19:21:28 +0800

    made the for loop, if condition and printted the output. Removd one comment

--- print_even_nums.py
+++ print_even_nums.py
@@ -1,4 +1,8 @@
 # Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
+
 # Make a For loop
-# Append only even nums
+for nums in range(0,101):
+    # Filter out even nums
+    if nums % 2 == 0:
 # Print even numbers
+        print(nums)
