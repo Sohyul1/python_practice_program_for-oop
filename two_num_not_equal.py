890ef9dedd780d90e10b3990913f3663aebc7946
Aeron <dequitoaeron353@gmail.com>
2025-03-12 20:52:39 +0800

    input 2 nums part

--- two_num_not_equal.py
+++ two_num_not_equal.py
@@ -1,4 +1,9 @@
 # Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+
 # Ask for user input
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
+
+
 # Determine if equal or not
 # Print the output
