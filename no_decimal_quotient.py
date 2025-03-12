f01893b5b05c0537a549d8cc8e6de08fe1844597
Aeron <dequitoaeron353@gmail.com>
2025-03-12 21:08:26 +0800

    input 2 nums part

--- no_decimal_quotient.py
+++ no_decimal_quotient.py
@@ -1,5 +1,9 @@
 # Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+
 # Ask for user input (float)
+num1 = float(input("Enter the dividend: "))
+num2 = float(input("Enter the divisor: "))
+
 # Get the quotient
 # Convert the quotient into integer
 # Print the quotient
