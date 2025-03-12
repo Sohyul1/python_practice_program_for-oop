958b7d731f9b147b7ac089f224ad137290c271c5
Aeron <dequitoaeron353@gmail.com>
2025-03-12 22:50:32 +0800

    For loop and print the output

--- printt_num_but_no_zero_and_five.py
+++ printt_num_but_no_zero_and_five.py
@@ -1,4 +1,8 @@
 # Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
+
 # Make a for loop
-# Filter out the number ending with 0 and 5
-# Print the rest of the numbers
+for num in range(0,101):
+    # Filter out the number ending with 0 and 5
+    if num % 10 != 0 and num % 5 != 0:
+        # Print the rest of the numbers
+        print(num)
