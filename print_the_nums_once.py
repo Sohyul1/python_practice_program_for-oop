fbebd7da6d694b49e6ed09d64b9f849dd2ae7075
Aeron <dequitoaeron353@gmail.com>
2025-03-13 00:22:24 +0800

    Resolved merge conflicts

--- print_the_nums_once.py
+++ print_the_nums_once.py
@@ -19,5 +19,13 @@ uniq = []
 for numbers in num_list:
     if num_list.count(numbers) == 1:
         uniq.append(numbers)
+<<<<<<< HEAD:print_the_nums_once.py
         
 # Print the numbers
+=======
+    else:
+        non_uniq.append(numbers)
+
+# Print the numbers
+print(uniq, non_uniq)
+>>>>>>> 00d93a6 (made the non uniq part and print the output, non uniq part still need adjustments):print_all_nums_once.py
