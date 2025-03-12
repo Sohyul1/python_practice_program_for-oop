cb2ef188e29214d0f905f26e5328118ff3c4a962
Aeron <dequitoaeron353@gmail.com>
2025-03-13 02:08:07 +0800

    pseudocode (had to repeat the code kasi hindi ko pala na ppush minsan

---
+++ continue_ask_lowest_highest.py
@@ -0,0 +1,6 @@
+# Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function
+# Make a count variable 
+# Make an empty list
+# Ask user for input (break if input is invalid)
+# Append the numbers in the list
+# Print the sorted list
