4e8aa848cf4407a57bfe2bddfda19b41cb22528c
Aeron <dequitoaeron353@gmail.com>
2025-03-12 21:40:55 +0800

    list and input numbers part

--- first_minus_remaining_nums.py
+++ first_minus_remaining_nums.py
@@ -1,5 +1,11 @@
 # Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
+
 # Make an empty list
+num_list = []
+
 # Ask user for input using for loop
+for ask in range(1, 11):
+    choice = float(input(f"Enter number {ask}: "))
+
 # Get the difference
 # Print the difference
