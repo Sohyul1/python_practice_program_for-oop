fa7546f4b9fba5b94de5f03ecce0041ac5581ace
Aeron <dequitoaeron353@gmail.com>
2025-03-12 23:12:46 +0800

    pseudocode

--- print_no_duplicate.py
+++ print_no_duplicate.py
@@ -1,5 +1,9 @@
+# Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
 # Make an empty list 
+
 # Ask for user input 
+
 # Append the number in the list 
+
 # Filter out numbers that appear only once 
 # Print the numbers that appear only once
