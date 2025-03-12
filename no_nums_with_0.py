5006e9969b04eac60d255f0e4d887939d8fd1b0b
Aeron <dequitoaeron353@gmail.com>
2025-03-12 19:36:07 +0800

    made the condition shorter

--- no_nums_with_0.py
+++ no_nums_with_0.py
@@ -1,8 +1,8 @@
 # Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
 
 # Make a for loop
-for nums in range(0,101):
+for nums in range(0, 101):
     # filter out the numbers with 0
-    if nums % 2 != 0 and nums % 5 != 0:
+    if nums % 10 != 0:
         # Print all the numbers
         print(nums)
