31571698450e2f188cc20bf4fc10a3241de34d27
Aeron <dequitoaeron353@gmail.com>
2025-03-12 22:00:14 +0800

    count and input ten numbers part

--- first_minus_remaining_nums.py
+++ first_minus_remaining_nums.py
@@ -1,7 +1,16 @@
 # Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
+
 # Make a count variable
+count = 1
+
 # Ask user for the 1st numer
+num1 = float(input("Enter number 1: "))
+
 # Use while loop to ask for the remaining numbers
+while count < 10:
+    rest_nums = float(input(f"Enter number {count + 1}: "))
+    count += 1
+
 # Get the difference
 # Print the difference
 
