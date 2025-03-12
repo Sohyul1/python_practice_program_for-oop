e0452c80fd2e637280989271c53213935a1eb830
Aeron <dequitoaeron353@gmail.com>
2025-03-12 18:35:47 +0800

    input 10 nums part

---
+++ sum_of_10_inputs.py
@@ -0,0 +1,8 @@
+# Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+
+# Ask for user input
+for ask in range (1,11):
+    ans = float(input(f"Enter number{ask}:"))
+
+# Get the sum of all of them
+# Print the sum 
