bb267f748a95faedb59ae6888089f6696c8294d6
Aeron <dequitoaeron353@gmail.com>
2025-03-12 23:28:26 +0800

    finished the backbone of code but not yet working properly

--- print_no_duplicate.py
+++ print_no_duplicate.py
@@ -1,13 +1,19 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
 
 # Make an empty list 
-unique_num = []
+num_list = []
 
 # Ask for user input 
 for ask in range(1, 11):
     nums = float(input(f"Enter number {ask}: "))
     # Append the number in the list 
-    unique_num.append(nums)
+    num_list.append(nums)
+
+# Make another lis for unique numbers
+unique_list = []
 
 # Filter out numbers that appear only once 
-# Print the numbers that appear only once
+if num_list.count(nums)== 1:
+    unique_list.append(nums)
+    # Print the numbers that appear only once
+    print(unique_list)
