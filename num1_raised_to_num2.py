0d57585d8ca169e96213d4f65f74d4062e6bc782
Aeron <dequitoaeron353@gmail.com>
2025-03-12 18:23:15 +0800

    pseudocode

---
+++ num1_raised_to_num2.py
@@ -0,0 +1,4 @@
+# Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+# Ask for user input 
+# Get the nth-square of the first name (second num is the exponent)
+# Print the value
