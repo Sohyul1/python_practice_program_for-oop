87778703e5df48d4081ae8d2754ef69625530012
Aeron <dequitoaeron353@gmail.com>
2025-03-12 22:09:59 +0800

    pseudo code

---
+++ even_counter.py
@@ -0,0 +1,6 @@
+# Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
+# Make an empty list
+# ASk for user input
+# Filter out the even nums
+# Get the taotal number of even
+# Print the total
