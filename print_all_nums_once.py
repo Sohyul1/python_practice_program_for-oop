48d696c3402f79fb51e3b395ba35ca4fc8eed48c
Aeron <dequitoaeron353@gmail.com>
2025-03-12 23:55:53 +0800

    made a list and input and appends

--- print_all_nums_once.py
+++ print_all_nums_once.py
@@ -1,7 +1,14 @@
 # Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
+
 # Make an emptry list
+num_list = []
+
 # Ask for user input
-# Append inputs into the list
+for ask in range(1, 11):
+    num = float(input(f"Enter number {ask}: "))
+    # Append inputs into the list
+    num_list.append(num)
+
 # Make another list for non-unique numbers
 # Make another list for unique numbers
 # Filter out the unique and not unique
