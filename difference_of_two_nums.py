e49d50b409e47115e49354f2461d718e5abe6deb
Aeron <dequitoaeron353@gmail.com>
2025-03-12 21:00:12 +0800

    input 2 nums part

--- difference_of_two_nums.py
+++ difference_of_two_nums.py
@@ -1,4 +1,8 @@
 # Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+
 # Ask for user input
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
+
 # Get the difference
 # Print the result
