cd3cb0fc30e7cb79ba79894e40a6c5099deae72c
Aeron <dequitoaeron353@gmail.com>
2025-03-12 18:56:39 +0800

    made the list and input 10 nums part

---
+++ odd_counter.py
@@ -0,0 +1,11 @@
+# Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
+
+# Make an empty list
+odd_list = []
+
+# Ask for user input
+for ask in range (1,11):
+    ans = float(input(f"Enter number{ask}:"))
+
+# Filter out the odd numbers 
+# Print the odd nums 
