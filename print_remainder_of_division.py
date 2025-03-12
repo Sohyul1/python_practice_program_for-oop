c1995bb65e87d8547b05a40fb620dc94ddcb47cb
Aeron <dequitoaeron353@gmail.com>
2025-03-12 21:20:12 +0800

    pseudocode

---
+++ print_remainder_of_division.py
@@ -0,0 +1,4 @@
+# Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
+# Ask for user inpu
+# Get the reaminder (use %)
+# Print the reaminder
