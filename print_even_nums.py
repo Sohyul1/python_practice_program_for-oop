58a7b89116478f2310bdca32de60975e3783fff8
Aeron <dequitoaeron353@gmail.com>
2025-03-12 20:35:05 +0800

    fixed some coding standard error

--- print_even_nums.py
+++ print_even_nums.py
@@ -1,6 +1,6 @@
 # Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
 
 # Make a For loop
-for nums in range(0,101, 2):
+for nums in range(0, 101, 2):
         # Print even numbers
         print(nums)
