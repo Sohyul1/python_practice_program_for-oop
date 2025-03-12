d9d01a5a435c243172f9f12b166aa9074f39699f
Aeron <dequitoaeron353@gmail.com>
2025-03-13 02:24:43 +0800

    hanggang dito po yung inabot ko before ko idelete yung una, you can check po sa commits ko it is titled working but keeps printing in real time por sir

--- continue_ask_lowest_highest.py
+++ continue_ask_lowest_highest.py
@@ -5,15 +5,23 @@ count = 1
 
 # Make an empty list
 num_list = []
+
 # Ask user for input (break if input is invalid)
 while True:
     try:
         number = float(input(f"Enter Number {count}: "))
         count += 1
 
-# Append the numbers into the list
-# Print the sorted list
+        # Append the numbers into the list
+        num_list.append(number)
 
+        # Sort the list
+        num_list.sort()
+
+        # Print the sorted list
+        print(num_list)
+    
     except ValueError:
+        print("Bye!")
         break
       
