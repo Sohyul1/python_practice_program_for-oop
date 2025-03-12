c2a17062e6f4c428764a468beaca3493c3c0fc43
Aeron <dequitoaeron353@gmail.com>
2025-03-12 22:24:28 +0800

    revised pseudocode

--- odd_nums_zero_to_hudred.py
+++ odd_nums_zero_to_hudred.py
@@ -1,6 +1,4 @@
 # Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
-# Make an empty list
-# Ask for user input using while loop
-# Filter out the odd numbers
-# Append odd numbers in the list
+# Use while lopp
+# Filter out the even numbers
 # Print the odd numbers
