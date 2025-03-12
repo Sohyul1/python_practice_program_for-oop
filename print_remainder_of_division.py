480c8f96be2d21dd387e3ccfe06db2d5002c8bd2
Aeron <dequitoaeron353@gmail.com>
2025-03-12 21:23:09 +0800

    input two nums part

--- print_remainder_of_division.py
+++ print_remainder_of_division.py
@@ -1,4 +1,9 @@
 # Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
-# Ask for user inpu
+
+# Ask for user input
+num1 = float(input("Enter the dividend: "))
+num2 = float(input("Enter the divisor: "))
+
 # Get the reaminder (use %)
+
 # Print the reaminder
