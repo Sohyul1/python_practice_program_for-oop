595db85bd5b1bb7aa994ad21ad304e90f4de1469
Aeron <dequitoaeron353@gmail.com>
2025-03-12 23:52:33 +0800

    pseudocode

---
+++ print_all_nums_once.py
@@ -0,0 +1,8 @@
+# Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
+# Make an emptry list
+# Ask for user input
+# Append inputs into the list
+# Make another list for non-unique numbers
+# Make another list for unique numbers
+# Filter out the unique and not unique
+# Print the numbers
