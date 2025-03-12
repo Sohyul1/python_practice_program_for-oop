3a3abc90090b61dc3da8b01c0f6f83aba7c1a686
Aeron <dequitoaeron353@gmail.com>
2025-03-13 01:20:04 +0800

    pseudocode

---
+++ continue_asking_display_lowest.py
@@ -0,0 +1,6 @@
+# Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
+# Make a count varibale
+# Make a list
+# Ask user for input (break if input is invalid)
+# Append the numbers in the list
+# Print the lowest
