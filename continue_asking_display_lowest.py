cf4035cb6051c4d503992790cf8a7dc1ccea96c9
Aeron <dequitoaeron353@gmail.com>
2025-03-13 01:23:43 +0800

    I just copied the firsst few parts from batch 3 prog 3 po sir since same silang continue asking

--- continue_asking_display_lowest.py
+++ continue_asking_display_lowest.py
@@ -1,6 +1,15 @@
 # Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
+
 # Make a count varibale
-# Make a list
+count = 1
+
+# Make a list 
+num_list = []
+
 # Ask user for input (break if input is invalid)
+while True:
+    try:
+        number = float(input(f"Enter Number {count}: "))
+        count += 1
 # Append the numbers in the list
 # Print the lowest
