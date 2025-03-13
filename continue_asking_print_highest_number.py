6fae77ad40fe5a67faa44d6fbb926b8b7c8193f8
Aeron <dequitoaeron353@gmail.com>
2025-03-13 08:01:50 +0800

    made a list and count, input numbers, and loop

--- continue_asking_print_highest_number.py
+++ continue_asking_print_highest_number.py
@@ -1,5 +1,17 @@
 # Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
+
 # Make an empty list
+num_list = []
+count = 1
 # Ask for user input
+while True:
+    try:
+        num = float(input(f"Enter number {count}: "))
+        count += 1
+
+    except ValueError:
+        break
+
 # Determine the highest number 
+
 # Print the highest number
