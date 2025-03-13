0986bc32e26cc60e68cefaa846c5689d1dda4e5d
Aeron <dequitoaeron353@gmail.com>
2025-03-13 08:28:36 +0800

    pseudocode

---
+++ continue_asking_print-average.py
@@ -0,0 +1,5 @@
+# Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
+# Makse an empty list
+# Ask for user input
+# Get the average
+# Print the average 
