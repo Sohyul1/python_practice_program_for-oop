0580f7c99e3ffd9577e0989e3e791d61780bd257
Aeron <dequitoaeron353@gmail.com>
2025-03-13 08:16:02 +0800

    input numbers using while loop, made the list a coun varible

--- continue_ask_high_to_low.py
+++ continue_ask_high_to_low.py
@@ -1,5 +1,20 @@
 # Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
+
 # Make an empty list
-# Ask for usr input
+num_list = []
+
+# make a count variable
+count = 1
+
+# Ask for user input
+while True:
+    try:
+        num = float(input(f"Enter number {count}: "))
+        num_list.append(num)
+        count += 1
+
+    except ValueError:
+        break
 # Use sort to arrange the numbers
+
 # Print arranged numbers
