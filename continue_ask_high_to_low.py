e579e2afeeaca94f534360450ff9560b7b72bbe8
Aeron <dequitoaeron353@gmail.com>
2025-03-13 08:12:02 +0800

    pseudocode

--- continue_ask_high_to_low.py
+++ continue_ask_high_to_low.py
@@ -1,5 +1,5 @@
 # Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
 # Make an empty list
 # Ask for usr input
-# Use sort to arrange
+# Use sort to arrange the numbers
 # Print arranged numbers
