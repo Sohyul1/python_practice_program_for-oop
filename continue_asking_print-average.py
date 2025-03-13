a197c4e18efc37026e72abb3843772fbab023027
Aeron <dequitoaeron353@gmail.com>
2025-03-13 08:31:43 +0800

    made list and count and input numbers just like the one form previous programs

--- continue_asking_print-average.py
+++ continue_asking_print-average.py
@@ -1,5 +1,17 @@
 # Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
-# Makse an empty list
+
+# Make an empty list
+num_list = []
+count = 1
+
 # Ask for user input
+while True:
+    try:
+        num = float(input(f"Enter number {count}; "))
+        count += 1
+    except ValueError:
+        break
+
 # Get the average
+
 # Print the average 
